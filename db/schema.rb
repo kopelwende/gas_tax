0e56b8eec5c7943da316a887e31a4fb36f9789df
kopelwende <kopelwende@gmail.com>
2022-12-09 03:02:07 +0900

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_072955) do
-
-  create_table "gasolines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.date "date", null: false
-    t.integer "prefectures_id", null: false
-    t.string "municipality", null: false
-    t.string "gas_station", null: false
-    t.integer "price", null: false
-    t.integer "liter", null: false
-    t.integer "main_gaso_tax", null: false
-    t.integer "provisional_gaso_tax", null: false
-    t.integer "local_main_gaso_tax", null: false
-    t.integer "local_provisional_gaso_tax", null: false
-    t.integer "consumption_tax", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_221_205_072_955) do
+  create_table 'gasolines', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.date 'date', null: false
+    t.integer 'prefectures_id', null: false
+    t.string 'municipality', null: false
+    t.string 'gas_station', null: false
+    t.integer 'price', null: false
+    t.integer 'liter', null: false
+    t.integer 'main_gaso_tax', null: false
+    t.integer 'provisional_gaso_tax', null: false
+    t.integer 'local_main_gaso_tax', null: false
+    t.integer 'local_provisional_gaso_tax', null: false
+    t.integer 'consumption_tax', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
